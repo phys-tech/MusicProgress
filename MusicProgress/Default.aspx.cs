221dbdf8a5b2e75d0aac0a8affb3a7e2cf4c7839
Ilya Denisov <ilya.denisov@fastdev.se>
2017-07-24 17:02:00 +0500

    [FIX] Check for current directory.

--- MusicProgress/Default.aspx.cs
+++ MusicProgress/Default.aspx.cs
@@ -14,11 +14,12 @@ namespace MusicProgress
         {
             //lFilenames.Text = dataCollector.allfiles;
 
-            lTotalFilesNum.Text = "HttpContext.Current.Request.Path = " + HttpContext.Current.Request.Path;
-            lTotalFilesNum.Text += "\nHttpContext.Current.Request.ApplicationPath = " + HttpContext.Current.Request.ApplicationPath;
-            lTotalFilesNum.Text += "\nHttpContext.Current.Request.Url.AbsolutePath = " + HttpContext.Current.Request.Url.AbsolutePath;
-            lTotalFilesNum.Text += "MapPath: " + Server.MapPath("\\App_Data\\");
-            
+            lTotalFilesNum.Text = "HttpContext.Current.Request.Path = " + HttpContext.Current.Request.Path + Environment.NewLine;
+            lTotalFilesNum.Text += "HttpContext.Current.Request.ApplicationPath = " + HttpContext.Current.Request.ApplicationPath + Environment.NewLine;
+            lTotalFilesNum.Text += "HttpContext.Current.Request.Url.AbsolutePath = " + HttpContext.Current.Request.Url.AbsolutePath + Environment.NewLine;
+            lTotalFilesNum.Text += "MapPath: " + Server.MapPath("\\App_Data\\") + Environment.NewLine;
+            lTotalFilesNum.Text += "Environmnet.CurrentDirectory: " + Environment.CurrentDirectory;
+
             System.Console.WriteLine("********************************************************");
             System.Console.WriteLine(HttpContext.Current.Request.Path);
             System.Console.WriteLine(HttpContext.Current.Request.ApplicationPath);
